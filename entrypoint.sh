8d6e7f30f68f4821d300b4b340876d533111e65a
Elton Kola <eltonkola@gmail.com>
2025-06-03 21:52:33 -0400

    update variable
    sdf

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 
-java -jar /app/bllok.jar "$INPUT_TEMPLATE" "$INPUT_INPUT" "$INPUT_OUTPUT"
+java -jar /app/bllok.jar "$INPUT_TEMPLATE" "$INPUT_INPUT" "$INPUT_OUTPUT" "$INPUT_ROOTPATH"
