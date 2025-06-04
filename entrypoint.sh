ae21df8e5f1a889a53a103c6e372fab8452d55a9
Elton Kola <eltonkola@gmail.com>
2025-06-04 10:40:53 -0400

    update jar

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-set -e
 
-java -jar /app/bllok.jar "$INPUT_TEMPLATE" "$INPUT_INPUT" "$INPUT_OUTPUT" "$INPUT_ROOTPATH"
+# Trust the GitHub workspace directory to avoid 'dubious ownership' errors
+git config --global --add safe.directory /github/workspace
+
+# Optional: trust all subdirectories (for extra safety if working in subfolders)
+find /github/workspace -type d -exec git config --global --add safe.directory {} \;
+
+# Then run your app
+exec java -jar /app/bllok.jar "$INPUT_TEMPLATE" "$INPUT_INPUT" "$INPUT_OUTPUT" "$INPUT_ROOTPATH"
