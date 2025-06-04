946720ed0ff91bb22f6f54b45a1a90963773113e
Elton Kola <eltonkola@gmail.com>
2025-06-04 11:30:14 -0400

    update jar

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,5 @@
 FROM eclipse-temurin:20-jdk-alpine
 
-# 🛠 Install Git
-RUN apk add --no-cache git
-
 COPY bllok.jar /app/bllok.jar
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 
-# Trust the GitHub workspace directory to avoid 'dubious ownership' errors
-git config --global --add safe.directory /github/workspace
-
-# Optional: trust all subdirectories (for extra safety if working in subfolders)
-find /github/workspace -type d -exec git config --global --add safe.directory {} \;
-
-# Then run your app
 exec java -jar /app/bllok.jar "$INPUT_TEMPLATE" "$INPUT_INPUT" "$INPUT_OUTPUT" "$INPUT_ROOTPATH"
